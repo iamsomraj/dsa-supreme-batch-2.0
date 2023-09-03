bbd7ffa80fb08beb4627f54de486163e9446373f
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-03 19:18:58 +0530

    👌 IMPROVE: added 2 week assignment 16

---
+++ 02-week-assignments/16-reverse-number/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int rev = 0;
+  while (n > 0) {
+    int lastDigit = n % 10;
+    rev = (rev * 10) + lastDigit;
+    n = n / 10;
+  }
+  cout << rev;
+  return 0;
+}
