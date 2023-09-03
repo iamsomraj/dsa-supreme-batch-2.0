7b53289ff1ec0d105fbd8494f84468e39e819d3f
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-03 19:08:47 +0530

    👌 IMPROVE: added 2 week assignment 14

---
+++ 02-week-assignments/14-check-prime/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int number;
+  cin >> number;
+  bool isPrime = true;
+  for (int i = 2; i * i <= number; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    cout << number << " is a prime number" << endl;
+  } else {
+    cout << number << " is not a prime number" << endl;
+  }
+  return 0;
+}
