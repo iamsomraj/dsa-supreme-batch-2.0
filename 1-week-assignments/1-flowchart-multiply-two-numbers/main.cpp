82c586401eb1e79559e113f9a2d1640248024aed
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-03 11:42:50 +0530

    👌 IMPROVE: added 1 week 1 fc

---
+++ 1-week-assignments/1-flowchart-multiply-two-numbers/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int firstNumber, secondNumber;
+  cin >> firstNumber >> secondNumber;
+  cout << firstNumber * secondNumber << endl;
+  return 0;
+}
