81a78b48154224cb0c86b634092f295352c19175
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-03 19:26:11 +0530

    👌 IMPROVE: added 2 week assignment 18

---
+++ 02-week-assignments/18-celcius-to-kelvin-and-fahrenheit/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  float c;
+  cin >> c;
+  cout << "Kelvin : " << (c + 273.15) << endl;
+  cout << "Fahrenheit : " << ((c * 1.8) + 32) << endl;
+}
