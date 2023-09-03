f6cd0209df73f12f94d149d21be864a8814f2f41
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-03 11:44:39 +0530

    👌 IMPROVE: added 1 week 2 fc

---
+++ 1-week-assignments/2-flowchart-perimeter-of-triangle/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int firstSide, secondSide, thirdSide;
+  cin >> firstSide >> secondSide >> thirdSide;
+  cout << firstSide + secondSide + thirdSide << endl;
+  return 0;
+}
