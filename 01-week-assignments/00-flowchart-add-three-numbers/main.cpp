9c5fb5825a8898e40dfbb6a2dc16c12dea07e0ee
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-04 23:50:09 +0530

    👌 IMPROVE: added outputts

---
+++ 01-week-assignments/00-flowchart-add-three-numbers/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int firstNumber, secondNumber, thirdNumber;
+  cin >> firstNumber >> secondNumber >> thirdNumber;
+  cout << (firstNumber + secondNumber + thirdNumber) << endl;
+  return 0;
+}
